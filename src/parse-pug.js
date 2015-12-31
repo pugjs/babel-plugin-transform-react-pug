8465dfce342175f9dcde63c94b3c831d3651da51
Forbes Lindesay <forbes@lindesay.co.uk>
2015-12-31 18:35:12 +0000

    Release 1.1.0

--- src/parse-pug.js
+++ src/parse-pug.js
@@ -1,7 +1,8 @@
 import lex from 'pug-lexer';
 import parse from 'pug-parser';
 import filters from 'pug-filters';
+import stripComments from 'pug-strip-comments';
 
 export default function (str) {
-  return filters.handleFilters(parse(lex(str)));
+  return filters.handleFilters(parse(stripComments(lex(str), {stripUnbuffered: true, stripBuffered: true})));
 }
