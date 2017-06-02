ad1a17dbe48aaa11df07755111fe63ee24698457
Forbes Lindesay <forbes@lindesay.co.uk>
2017-06-02 16:51:03 +0100

    Remove based from snapshots

---
+++ scripts/filename-serializer.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const basedir = path.resolve(__dirname + '/..').replace(/\\/g, '/');
+
+function matchesBasedir(value) {
+  return typeof value === 'string' && value.replace(/\\/g, '/').indexOf(basedir) !== -1;
+}
+function removeBasedir(value) {
+  const index = value.replace(/\\/g, '/').indexOf(basedir);
+  return (
+    value.substr(0, index) +
+    '<basedir>' +
+    value
+      .substr(index + basedir.length)
+      .replace(/^[\/a-z\-\_\.]+/g, _ => _.replace(/\\/g, '/'))
+  );
+}
+// filename serializer that removes the basedir
+module.exports = {
+  test: function(val) {
+    return matchesBasedir(val);
+  },
+  print: function(val, serialize, indent) {
+    return serialize(removeBasedir(val));
+  }
+};
