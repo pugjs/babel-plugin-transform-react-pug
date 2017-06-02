1525c04579c4cf0ed0931393b8d8e687813802b1
Forbes Lindesay <forbes@lindesay.co.uk>
2017-06-02 13:50:32 +0100

    Add lots more test cases

---
+++ src/__tests__/each-else.input.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = pug`
+  div
+    each list, i in [[1, 2, 3], [], null]
+      div(key=i)
+        each item in list
+          span(key=item)= item
+        else
+          | No items in this list
+        each item in list
+          span(key=item)= item
+        else
+          | This is #[strong literally] the same list!
+`;
---
+++ src/__tests__/if-else.input.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = pug`
+  div
+    each v, i in [1, 2, 3]
+      div(key=i)
+        if v === 1
+          | One
+        else if v === 2
+          | Two
+        else
+          | Many
+        if v === 3
+          strong Three
+          | is the magic number!
+        if v ==  2
+        else
+          strong Definitely
+          | not 2
+`;
