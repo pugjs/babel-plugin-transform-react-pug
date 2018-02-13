4e0d27ac3f546e9ed8d94c22de7ec7df3087332f
Eugene Zhlobo <ezhlobo@gmail.com>
2018-02-13 12:30:01 +0300

    Update eslint and flow-bin dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,14 @@
-module.exports = {extends: "forbeslindesay"};
+module.exports = {
+  extends: [
+    "forbeslindesay",
+    "plugin:flowtype/recommended"
+  ],
+  plugins: [
+    "flowtype"
+  ],
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true
+    }
+  }
+};
