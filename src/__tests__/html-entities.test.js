18fa72b3f4cdb403da6f35950f746913bd6322a4
Eugene Zhlobo <ezhlobo@gmail.com>
2018-03-20 21:26:42 +0300

    Add tests to check handling of xhtml entities (named and numeric)

---
+++ src/__tests__/html-entities.test.js
@@ -0,0 +1,22 @@
+import {transformFileSync} from 'babel-core';
+import transformReactPug from '../';
+import transformReactJsx from 'babel-plugin-transform-react-jsx';
+
+const FILENAME = __dirname + '/html-entities.input.js';
+
+const transform = (...plugins) =>
+  transformFileSync(FILENAME, {
+    babelrc: false,
+    compact: false,
+    plugins: [transformReactPug, ...plugins],
+  }).code;
+
+test('JavaScript output', () => {
+  const src = transform();
+  expect(src).toMatchSnapshot('transformed source code');
+});
+
+test('JSX output', () => {
+  const src = transform(transformReactJsx);
+  expect(src).toMatchSnapshot('generated JSX');
+});
