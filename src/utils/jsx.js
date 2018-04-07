1d48f781ccd8381d3dccd654405af0381f0c4400
Louie Colgan <ljbc94@gmail.com>
2018-04-07 09:38:12 +0100

    chore(comment): add todo to update building jsx fragment

--- src/utils/jsx.js
+++ src/utils/jsx.js
@@ -20,6 +20,8 @@ export function buildJSXElement(
   return t.jSXElement(open, close, children, noChildren);
 }
 
+// TODO: This can be replaced when migrating to Babel 7 as JSXFragment
+// has been added in v7.0.0-beta.30.
 export function buildJSXFragment(children: JSXChildren): JSXElement {
   const fragmentExpression = t.jSXMemberExpression(
     t.jSXIdentifier('React'),
