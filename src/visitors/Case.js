fba16baa1e51e214b1d8ef7e3c53ba0c6a2ae2e9
Eugene Zhlobo <ezhlobo@gmail.com>
2019-01-06 19:47:17 +0300

    Wrap static blocks by fragment (#84)
    * Request desired behavior in tests
    
    * Remove own keys from generating code

--- src/visitors/Case.js
+++ src/visitors/Case.js
@@ -1,6 +1,7 @@
 // @flow
 
 import parseExpression from '../utils/parse-expression';
+import {buildJSXFragment} from '../utils/jsx';
 import type Context from '../context';
 import t from '../lib/babel-types';
 import {visitExpressions} from '../visitors';
@@ -23,7 +24,7 @@ function convertCases(
       if (children.length === 0) {
         return t.identifier('null');
       }
-      return t.arrayExpression(children);
+      return buildJSXFragment(children);
     },
   );
   if (node.expr === 'default') {
